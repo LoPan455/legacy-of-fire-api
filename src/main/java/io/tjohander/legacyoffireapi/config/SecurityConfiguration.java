9e9bf1d7a7db4defa6c9b33509ff37a8826fc965
Thomas Johander <tom.johander@dealerteamwork.com>
2020-02-14 07:01:25 -0600

    Added in basic in-memory user database with sample user for testing purposes

---
+++ src/main/java/io/tjohander/legacyoffireapi/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package io.tjohander.legacyoffireapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    User.UserBuilder users = User.withDefaultPasswordEncoder();
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    manager.createUser(users.username("thomas").password("password").roles("ADMIN").build());
+    return manager;
+  }
+}
