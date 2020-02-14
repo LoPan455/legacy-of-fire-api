0d71fab53d86b027277ffbd25feba5c07b719fe0
Thomas Johander <tom.johander@dealerteamwork.com>
2020-02-13 21:38:41 -0600

    Initial Commit

---
+++ src/main/java/io/tjohander/legacyoffireapi/LegacyOfFireApiApplication.java
@@ -0,0 +1,13 @@
+package io.tjohander.legacyoffireapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyOfFireApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LegacyOfFireApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/tjohander/legacyoffireapi/LegacyOfFireApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tjohander.legacyoffireapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyOfFireApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
